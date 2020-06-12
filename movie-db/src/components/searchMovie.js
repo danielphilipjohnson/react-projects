7624fb4863086738c64daf8180a5dc60ab6ec52b
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-06-12 21:03:21 +0100

    Created Movie search and display as cards

--- movie-db/src/components/searchMovie.js
+++ movie-db/src/components/searchMovie.js
@@ -1,22 +1,63 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from "react";
 
-class SearchMovie extends React.Component {
-  render() {
-    return (<form className="form">
-      <label className="label" htmlFor="query">
-        Movie Name
-      </label>
-      <input
-        className="input"
-        type="text"
-        name="query"
-        placeholder="i.e. Jurassic Park"
-      />
+export default function SearchMovie() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
 
-      <button className="button">Search</button>
-    </form>)
-  }
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("called");
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="i.e. Jurassic Park"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+
+        <button className="button">Search</button>
+        <div className="card-list">
+          {movies
+            .filter((movie) => movie.poster_path)
+            .map((movie) => (
+              <div className="card" key={movie.id}>
+                <img
+                  className="card--image"
+                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                  alt={movie.title + " poster"}
+                />
+                <div className="card--content">
+                  <h3 className="card--title">{movie.title}</h3>
+                  <p>
+                    <small>RELEASE DATE: {movie.release_date}</small>
+                    <small>Rating: {movie.vote_average}</small>
+
+                  </p>
+            <p className="card--desc">{movie.overview}</p>
+                </div>
+              </div>
+            ))}
+        </div>
+      </form>
+    </>
+  );
 }
-
-export default SearchMovie;
