8357b258c3fa3487ba143e7f37efabd4a9181bf6
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-10-27 21:33:47 +0000

    Added personal portfolio converted to react

---
+++ portfolio/personal-fcc-portfolio/build/precache-manifest.3f02438c72265ee7810f1ceee66c16b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fa3e5ccd1b5129cb5720dbd69bff3c",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "5edcf0c457bfda1d953d",
+    "url": "/react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e7365dda37efbd38ccc",
+    "url": "/react-portfolio/static/js/2.03cad01a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.03cad01a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5edcf0c457bfda1d953d",
+    "url": "/react-portfolio/static/js/main.79895cb1.chunk.js"
+  },
+  {
+    "revision": "bee14ebf850387594d39",
+    "url": "/react-portfolio/static/js/runtime-main.8c64cdeb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
