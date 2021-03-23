e02a3ea59090f27a5e127179d4f4f4cb8460cba6
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-03-23 11:08:46 +0000

    setup basic project

---
+++ Typescript/Using-ts/public/js/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var foo = "test";
+var bar = 1;
+var baz = ["This", "is", "a", "Test"];
+var myFunc = function () {
+    console.log("im working");
+};
+myFunc();
---
+++ Typescript/Using-ts/src/index.ts
@@ -0,0 +1,9 @@
+const container: HTMLElement | any = document.getElementById("app");
+
+
+
+const myFunc = (): void => {
+  console.log("im working");
+};
+
+myFunc();
