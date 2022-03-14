53c6e0d82198ab417361da9d51edaf71fb73f43a
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-03-14 22:06:46 +0000

    Chore added firebase

---
+++ NextJS/next-dev-to/lib/firebase.ts
@@ -0,0 +1,21 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQOtUGiF9VHsfkXW66GMD_rFGeAGSyS_E",
+  authDomain: "devto-1816d.firebaseapp.com",
+  projectId: "devto-1816d",
+  storageBucket: "devto-1816d.appspot.com",
+  messagingSenderId: "133201442579",
+  appId: "1:133201442579:web:ecc58bbc6384ff675c0ce6",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
