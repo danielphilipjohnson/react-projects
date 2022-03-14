238e77bd4a887a74dbab1903c01f2ecec510ca1f
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-03-14 21:29:43 +0000

    simple car page with error handling

---
+++ NextJS/next-temp/pages/cars/[id].js
@@ -0,0 +1,49 @@
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import styles from '../../styles/Home.module.css'
+
+export default function Car({ car, error }) {
+
+	const router = useRouter()
+	const { id } = router.query
+	
+	if (error) {
+		return <div>Error! {error.message}</div>
+	}
+
+	return (
+		<div className={styles.container}>
+
+			<Head>
+				<title>{car.color} {car.id}</title>
+			</Head>
+
+			<main className={styles.main}>
+				<h1 className={styles.title}>
+					{id}
+				</h1>
+
+				<img src={car.image} width="300px" />
+
+			</main>
+		</div>
+	)
+}
+
+
+export async function getServerSideProps({ params }) {
+	const req = await fetch(`http://localhost:3000/${params.id}.json`);
+	if (req.status !== 200) {
+		return {
+			props: { error: { message: "it isnt working" } }
+		}
+	}
+	else {
+		const data = await req.json();
+
+		return {
+			props: { car: data },
+		}
+	}
+
+}
