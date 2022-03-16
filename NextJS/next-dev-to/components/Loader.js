05ecefe2cd2c3610bd5d64339b67f4f93fac1b44
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-03-16 18:47:47 +0000

    Updated loader to tailwind

--- NextJS/next-dev-to/components/Loader.js
+++ NextJS/next-dev-to/components/Loader.js
@@ -1,4 +1,4 @@
 // Loading Spinner
 export default function Loader({ show }) {
-	return show ? <div className="loader"></div> : null;
+	return show ? <div className="border-b-slate-300 border-l-slate-300 border-r-slate-300 border-8 rounded-3xl w-12 h-12 border-t-emerald-300 animate-spin"></div> : null;
 }
